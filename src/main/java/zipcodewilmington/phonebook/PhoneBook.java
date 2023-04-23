456d39f184522c75817903bb1e406d1375afc225
JerryFlop <JParedes@wil.yearup.org>
2023-04-23 16:41:17 -0400

    commit

--- src/main/java/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -16,11 +13,11 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook = map;
     }
 
     public PhoneBook() {
-        this(null);
+        this(new HashMap<>());
     }
 
     public void add(String name, String phoneNumber) {
